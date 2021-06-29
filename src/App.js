2b9c2936011a61d1f31f7e0312b7c0840aa64191
iamtreetop <trivta@gmail.com>
2021-06-28 22:15:52 -0700

    Add useEffect to App.js for event and month change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./components/header/Header";
 import Day from "./components/day/Day";
@@ -8,8 +8,73 @@ import "./App.css";
 function App() {
   // to navigate months, 0 is current, -1 for prev, +1 for next
   const [currentMonth, setCurrentMonth] = useState(0);
+  // trigger day
+  const [clicked, setClicked] = useState();
   // initalize events variables; will save to local storage
   const [events, setEvents] = useState([]);
+  // store day obvjects
+  const [days, setDays] = useState([]);
+  // month year display date
+  const [dateDisplay, setDateDisplay] = useState("");
+
+  useEffect(() => {
+    const weekdays = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
+    const date = new Date();
+
+    if (currentMonth !== 0) {
+      date.setMonth(new Date().getMonth() + currentMonth);
+    }
+
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+
+    const firstOfMonth = new Date(year, month, 1);
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+    const dateString = firstOfMonth.toLocaleDateString("en-us", {
+      weekday: "long",
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+    });
+
+    setDateDisplay(`${date.toLocaleDateString("en-us", { month: "long" })}`);
+    const fillerDays = weekdays.indexOf(dateString.split(", ")[0]);
+
+    // build days array
+    const daysArray = [];
+    for (let i = 1; i <= fillerDays + daysInMonth; i++) {
+      const dayString = `${month + 1} / ${i - fillerDays} / ${year}`;
+
+      if (i > fillerDays) {
+        // not filler day; push day objects
+        daysArray.push({
+          value: i - fillerDays,
+          isCurrentDay: i - fillerDays === day && currentMonth === 0,
+          date: dayString,
+        });
+      } else {
+        daysArray.push({
+          value: "filler",
+          event: null,
+          isCurrentDay: false,
+          date: "",
+        });
+      }
+    }
+
+    setDays(daysArray);
+  }, [events, currentMonth]);
 
   return (
     <div id="container">
